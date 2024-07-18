4c9054b796d0db0556951349740ca6a54615b0ac
gabrieldu4rte <gabrielduartedt69@gmail.com>
2024-07-18 20:42:32 -0300

    feat: implementing composer with twig and configuring the views to dynamically include the content from the controller

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -5,8 +5,15 @@ class HomeController{
 
         try {
             $colecPostagens = Postagem::selecionaTodos();
+            $loader = new \Twig\Loader\FilesystemLoader('app/View');
+            $twig = new \Twig\Environment($loader);
+            $template = $twig->load('home.html');
 
-            var_dump($colecPostagens);
+            $parametros = array();
+            $parametros['postagens'] = $colecPostagens;
+            $conteudo = $template->render($parametros);
+            echo $conteudo;
+
         } catch (Exception $e) {
             echo $e->getMessage();
         }
