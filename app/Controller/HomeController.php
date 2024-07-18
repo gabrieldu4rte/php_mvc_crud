1ef5eaabaff09e03e558e793403472d1f86d4d7e
gabrieldu4rte <gabrielduartedt69@gmail.com>
2024-07-18 20:01:29 -0300

    config: initializing the project with html template and cores/controllers

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+class HomeController{
+    public function index(){
+
+        try {
+            $colecPostagens = Postagem::selecionaTodos();
+
+            var_dump($colecPostagens);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        
+    }
+}
